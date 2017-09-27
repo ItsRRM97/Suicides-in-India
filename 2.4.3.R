cea514ba6449dd92f44b9a523f412c075fef243d
Roshan Raj Mishra <mishra.roshanraj@gmail.com>
2017-09-27 19:20:13 +0530

    State Wise Professional Profile and Social Status as causes of Suicide

---
+++ 2.4.3.R
@@ -0,0 +1,39 @@
+# State wise profiles of suicide
+
+stateProfile <- function(Dataset) {
+  
+  profile <- vector() # Null Vector
+  Dataset$Type_code <- as.character(Dataset$Type_code)
+  Dataset$Type <- as.character(Dataset$Type)
+  for(i in 1:236583) {
+    if(Dataset$Type_code[i] == 'Professional_Profile') {
+      profile<-c(profile,Dataset$Type[i])
+    }
+  }
+  profile <- unique(profile)
+  profile <- sort(profile)
+  
+  state <- c(unique(as.character((Dataset$State))))
+  
+  mat <- matrix(0, nrow = length(state), ncol = length(profile))
+  dimnames(mat) <- list(state,1:length(profile))
+  
+  #making a legend for readability
+  legend <- data.frame(list(1:length(profile)),profile)
+  
+  for(i in 1:236583) {
+    if(Dataset$Total[i] != 0 && Dataset$Type_code[i] == 'Professional_Profile') {
+      for(j in 1:length(state)) {
+        if(Dataset$State[i] == state[j]) {
+          for(k in 1:length(profile)) {
+            if(Dataset$Type[i] == profile[k]) {
+              mat[j,k] = mat[j,k] + Dataset$Total[i]
+            }
+          }
+        }
+      }
+    }
+  }
+  print(mat)
+  print(legend)
+}
---
+++ 2.4.4.R
@@ -0,0 +1,39 @@
+# State wise social_status of suicide
+
+stateSocial <- function(Dataset) {
+  
+  social_stat <- vector() # Null Vector
+  Dataset$Type_code <- as.character(Dataset$Type_code)
+  Dataset$Type <- as.character(Dataset$Type)
+  for(i in 1:236583) {
+    if(Dataset$Type_code[i] == 'Social_Status') {
+      social_stat<-c(social_stat,Dataset$Type[i])
+    }
+  }
+  social_stat <- unique(social_stat)
+  social_stat <- sort(social_stat)
+  
+  state <- c(unique(as.character((Dataset$State))))
+  
+  mat <- matrix(0, nrow = length(state), ncol = length(social_stat))
+  dimnames(mat) <- list(state,social_stat)
+  
+  #making a legend for readability
+  #legend <- data.frame(list(1:length(social_stat)),social_stat)
+  
+  for(i in 1:236583) {
+    if(Dataset$Total[i] != 0 && Dataset$Type_code[i] == 'Social_Status') {
+      for(j in 1:length(state)) {
+        if(Dataset$State[i] == state[j]) {
+          for(k in 1:length(social_stat)) {
+            if(Dataset$Type[i] == social_stat[k]) {
+              mat[j,k] = mat[j,k] + Dataset$Total[i]
+            }
+          }
+        }
+      }
+    }
+  }
+  print(mat)
+  #print(legend)
+}
