575b41f65062c08673d0b826475bb8adaaf63e9b
Roshan Raj Mishra <mishra.roshanraj@gmail.com>
2017-09-28 01:50:57 +0530

    Complete set of Year wise Analysis of Suicides in India

---
+++ 3.3.4.R
@@ -0,0 +1,39 @@
+# year wise social status of suicide
+
+yearSocial <- function(Dataset) {
+  
+  social <- vector() # Null Vector
+  Dataset$Type_code <- as.character(Dataset$Type_code)
+  Dataset$Type <- as.character(Dataset$Type)
+  for(i in 1:236583) {
+    if(Dataset$Type_code[i] == 'Social_Status') {
+      social<-c(social,Dataset$Type[i])
+    }
+  }
+  social <- unique(social)
+  social <- sort(social)
+  
+  year <- c(unique(as.character((Dataset$Year))))
+  
+  mat <- matrix(0, nrow = length(year), ncol = length(social))
+  dimnames(mat) <- list(year,social)
+  
+  #making a legend for readability
+  #legend <- data.frame(list(1:length(social)),social)
+  
+  for(i in 1:236583) {
+    if(Dataset$Total[i] != 0 && Dataset$Type_code[i] == 'Social_Status') {
+      for(j in 1:length(year)) {
+        if(Dataset$Year[i] == year[j]) {
+          for(k in 1:length(social)) {
+            if(Dataset$Type[i] == social[k]) {
+              mat[j,k] = mat[j,k] + Dataset$Total[i]
+            }
+          }
+        }
+      }
+    }
+  }
+  print(mat)
+  #print(legend)
+}
---
+++ 3.3.5.R
@@ -0,0 +1,39 @@
+# year wise edu_stat of suicides
+
+yearEducation <- function(Dataset) {
+  
+  edu_stat <- vector() # Null Vector
+  Dataset$Type_code <- as.character(Dataset$Type_code)
+  Dataset$Type <- as.character(Dataset$Type)
+  for(i in 1:236583) {
+    if(Dataset$Type_code[i] == 'Education_Status') {
+      edu_stat<-c(edu_stat,Dataset$Type[i])
+    }
+  }
+  edu_stat <- unique(edu_stat)
+  edu_stat <- sort(edu_stat)
+  
+  year <- c(unique(as.character((Dataset$Year))))
+  
+  mat <- matrix(0, nrow = length(year), ncol = length(edu_stat))
+  dimnames(mat) <- list(year,1:length(edu_stat))
+  
+  #making a legend for readability
+  legend <- data.frame(list(1:length(edu_stat)),edu_stat)
+  
+  for(i in 1:236583) {
+    if(Dataset$Total[i] != 0 && Dataset$Type_code[i] == 'Education_Status') {
+      for(j in 1:length(year)) {
+        if(Dataset$Year[i] == year[j]) {
+          for(k in 1:length(edu_stat)) {
+            if(Dataset$Type[i] == edu_stat[k]) {
+              mat[j,k] = mat[j,k] + Dataset$Total[i]
+            }
+          }
+        }
+      }
+    }
+  }
+  print(mat)
+  print(legend)
+}
