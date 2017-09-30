b285ec9b7a402a15d082b5a5dc0359c183bf57b3
Roshan Raj Mishra <mishra.roshanraj@gmail.com>
2017-09-30 22:36:10 +0530

    Add Graph for 1.1

--- 1.1.R
+++ 1.1.R
@@ -17,10 +17,10 @@ SuicideYearly <- function(Dataset)
   }
   write.table(frame,"output/1.1.csv", row.names = TRUE, sep = ",")
   
-  # bar graph
+  # bar graphS
   
   png(file = "plots/yearly_suicides.png")
   barplot(frame$no_of_suicides/100000,names.arg = year,xlab = "Years",ylab = "Suicides(in lakhs)",col = "blue",
-          main = "Suicides over the Years",border = "red")
+          main = "Suicides over the Years")
   dev.off()
 }
